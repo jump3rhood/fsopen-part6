8ba2a07795b29803ed3a977fb9b97372f5abb699
jump3rhood <mjm13121@gmail.com>
2023-02-27 13:27:52 +0530

    ex 6.3 & 6.4 anecdotes step 1:upvote func, step 2: create new func

---
+++ redux-anecdotes/src/App.js
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { createAnecdote, upVote } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(upVote(id))
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const anecdote = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(anecdote))
+  }
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={handleSubmit}>
+        <div><input name='anecdote'/></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
