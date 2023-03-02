d617e88ffabe88bea6cad4f70ec90e17a53ef050
jump3rhood <mjm13121@gmail.com>
2023-03-02 18:06:31 +0530

    ex 6.10 contd implement filterReducer using createSlice

--- redux-anecdotes/src/index.js
+++ redux-anecdotes/src/index.js
@@ -4,7 +4,6 @@ import store from './reducers/store'
 import { Provider } from 'react-redux'
 import App from './App'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
