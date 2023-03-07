2ab08a3c858fad2f4663ef4dac038ec93c8e7d7b
jump3rhood <mjm13121@gmail.com>
2023-03-07 22:56:14 +0530

    ex 6.21 implement adding new anecdote using React-Query

--- query-anecdotes/src/components/AnecdoteForm.js
+++ query-anecdotes/src/components/AnecdoteForm.js
@@ -1,10 +1,18 @@
+import { QueryClient, useMutation } from "react-query"
+import { createAnecdote } from "../requests"
 const AnecdoteForm = () => {
-
+  const queryClient = new QueryClient()
+  const newMutation = useMutation(createAnecdote)
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
+    newMutation.mutate({ content, votes: 0}, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('anecdotes')
+      }
+    })
 }
 
   return (
--- query-anecdotes/src/requests.js
+++ query-anecdotes/src/requests.js
@@ -2,3 +2,7 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:3001/anecdotes'
 
 export const getAll = () => axios.get(baseUrl).then(res => res.data)
+export const createAnecdote = (object) => 
+  axios
+    .post(baseUrl, object)
+    .then(res => res.data)
